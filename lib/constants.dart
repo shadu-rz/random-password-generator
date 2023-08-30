7835a30962b123ad9572acf6d629209e7b443417
shadu-rz <mshadulykk76@gmail.com>
2023-08-30 15:44:28 +0530

    textfield added

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  final primaryColor = Colors.teal;
+  final secondaryColor = Colors.red;
+  
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_password/constants.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -10,9 +11,49 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title:const Text('Secure password'),
+        centerTitle: true,
+        title: Text(
+          'Secure password',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants().primaryColor,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Random Secure password',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: ColorConstants().secondaryColor,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              readOnly: true,
+              decoration: InputDecoration(
+                hintText: 'your password will shown here',
+                enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: ColorConstants().primaryColor),
+                    borderRadius: BorderRadius.circular(10)),
+                focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: ColorConstants().primaryColor,
+                    ),
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
